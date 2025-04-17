65affd11e2cb133f2e49c53e4e3d6a7fafffe9d7
Adriangarba <boubuk@myrecovery.casa>
2025-04-17 01:35:14 -0700

    cron job added

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 // server.js
 const express = require('express');
 const path = require('path');
-
+const cron = require("node-cron")
+const axios = require("axios")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get("/cron", (req,res) => {
+app.get("/cron", (req, res) => {
     res.send("ok")
 })
 
+const keepAwake = async () => {
+    try {
+        const response = await axios.get("http://localhost:3000/cron")
+        console.log(response.data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+const job = cron.schedule("*/1 * * * *", keepAwake)
+
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
