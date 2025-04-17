3bbf8c880af66d42526d404a25c62cb29d275537
Adriangarba <boubuk@myrecovery.casa>
2025-04-17 00:51:04 -0700

    added cron routes

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get("/cron", (req,res) => {
+    res.send("ok")
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
